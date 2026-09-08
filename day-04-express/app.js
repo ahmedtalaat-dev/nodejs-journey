395944c7842bec64fe025724ed444eb762f2605c
Ahmed Talaat <ahmed.talaat.ali05@gmail.com>
2026-09-08 18:26:35 +0300

    feat: add students crud api

---
+++ day-04-express/app.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const app = express();
+app.use(express.json())
+
+const students = [
+  {
+    id: 1,
+    name: "Ahmed",
+    age: 21,
+  },
+  {
+    id: 2,
+    name: "Ali",
+    age: 21,
+  },
+  {
+    id: 3,
+    name: "Omar",
+    age: 33,
+  },
+  {
+    id: 4,
+    name: "Mona",
+    age: 31,
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send("Hello Ahmed");
+});
+
+app.get("/api/students", (req, res) => {
+    res.json(students)
+})
+
+app.get("/api/students/:studentId", (req, res) => {
+    const id = req.params.studentId;
+    const student = students.filter((s) => {
+        return s.id == id;
+    })
+    res.json(student)
+})
+
+app.post("/api/students", (req, res) => {
+    const data = req.body;
+    const newStudent = {...data, id: `${students.length +1}`}
+    students.push(newStudent)
+    res.json(students)
+})
+
+app.delete("/api/students/:studentId", (req, res) => {
+    const id = Number(req.params.studentId);
+    const newStudents = students.filter((s) => {
+        return s.id !== id;
+    })
+    res.json(newStudents)
+})
+
+app.put("/api/students/:studentId", (req, res) => {
+  const id = Number(req.params.studentId);
+  const data = req.body;
+
+  const index = students.findIndex((s) => {
+    return s.id === id;
+  });
+
+  if (index === -1) {
+    return res.status(404).json({ message: "Student not found" });
+  }
+
+  students[index] = {
+    id: id,
+    ...data
+  };
+
+  res.json(students);
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
